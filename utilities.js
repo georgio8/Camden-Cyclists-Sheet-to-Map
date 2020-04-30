292dfa88aae9bf5bc1130f7472dc7b07cd185a8e
George Coulouris <george@coulouris.net>
2020-04-30 14:34:21 +0100

    Synchronise with currently deployed version

--- utilities.js
+++ utilities.js
@@ -1,1 +1,52 @@
-/* Contants and utility functions for use in Sheet-to-Map app */
+/* Constants and utility functions for use in Sheet-to-Map app */
+
+	const LatColumnName = "Latitude", LngColumnName = "Longitude", SVColumnName = "StreetView";
+//	Quick Wins
+//	const SheetUrl = 'https://docs.google.com/spreadsheets/d/1HvqP8O6oZFB8s70obKc9M_ksk-QcYKF6kxxcmqJ9HUg/edit?usp=sharing';
+//	Covid19 shopping issues
+	const SheetUrl = 'https://docs.google.com/spreadsheets/d/1B6Sj6RaSF1Hf43ESlnVxNdO1HXuwPNcxU7tSlxqqFig/edit?usp=sharing';
+	const FieldsToShow = ["Title", "Location", "Description", "Photos", "StreetView"];
+	const FieldSyles = {"Title": "H3", "Location": "H4", "Description": "p","Photos": "links","StreetView": "links"};
+	const MapInitialCenter = [-0.16, 51.543]; // starting position [lng, lat]
+	const MapInitialZoom = 12.5; // starting zoom
+	function urlParam(name){
+	// get a named parameter from the URL string
+		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+		if (results==null){
+		   return null;
+		}
+		else{
+		   return decodeURIComponent(results[1]) || 0;
+		}
+	}
+
+	function getLngLat(row) {
+		if(row[LatColumnName] && row[LngColumnName]) {
+			return [[LatColumnName], row[LngColumnName]];
+		} else if(row[SVColumnName] && row[SVColumnName].indexOf('@') > 0) {
+			return extractStreetViewLocation(row[SVColumnName]);
+		} else {
+			return null;
+		}
+	}
+		
+	  function extractStreetViewLocation(googleURL) {
+			var s = googleURL.slice(googleURL.indexOf('@')+1);
+			var parts = s.split(',');
+			return [parts[1],parts[0]];
+	  }
+
+	  function getURLParams() {
+	  	var latName, lngName, svn;
+		if(t = urlParam("title")) document.title = t;
+		if((latName = urlParam("latcolumn")) && (lngName = urlParam("lngcolumn"))) {
+			LatColumnName = latName; LngColumnName = urlParam("latcolumn") ;
+		}
+		if(svn = urlParam("svcolumn")) SVColumnName = svn;
+	  }
+	  
+	  function categoryToColor(category) {
+	  	return ((category == "Shops") ? "red" : 
+	  		(category == "Filter") ? "green" :
+	  		(category == "MainRoad") ? "orange" : "white");
+	  }
